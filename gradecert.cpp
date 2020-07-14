befd92f9708ec9a7774834c568dfa2f1fd4c6868
Humchum <steamer2544@gmail.com>
2020-07-14 16:13:20 +0700

    intitial commit

---
+++ gradecert.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << " 1st Term of 2020\t" << "/" << "45132114";
+	cout << "\tRatthakorn Polsaen\n" << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "| Subjcect ID |" << " Subject name\t" << "\t | Score (100)  |" << " Grad  |";
+	cout << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "|   4570034   |" << " Introduction to computer |" << "\t85\t|" << "  'A'  |" << endl;
+	cout << "|   4580247   |" << " English                  |" << "\t74\t|" << "  'B'  |" << endl;
+	cout << "|   4530001   |" << " Design Thinking          |" << "\t92\t|" << "  'A'  |" << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "|    Total    |" << " Subject = 3              |" << "    Average = 3.87\t|" << endl;
+	cout << "========================================*=====" << endl;
+	cout << endl << endl;
+	cout << "KMUTNB" << endl;
+	system("pause");
+	return 0;
+}
