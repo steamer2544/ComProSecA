f5669f753ec459020a4050bf174216cb88b1cbd8
steamer2544 <steamer2544@gmail.com>
2020-07-21 13:56:03 +0700

    initial commit

---
+++ Ex4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    string name;
+    string surname;
+    float salary;
+    float sale;
+    float comper;
+    float total;
+
+    cout << "****Homework 2************" << endl;
+    cout << "Enter Name   : ";
+    cin >> name;
+    cin >> surname;
+    cout << "Enter Salary : ";
+    cin >> salary;
+    cout << "Enter Sale   : ";
+    cin >> sale;
+    cout << "Enter Commission Percent : ";
+    cin >> comper;
+    cout << "------output----------" << endl;
+    cout << "You name = " << name << " " <<surname << endl;
+    total = salary + (sale*(comper/100));
+    cout << "Total Revenue  " << total << "Baht" << endl;
+
+    return 0;
+}
